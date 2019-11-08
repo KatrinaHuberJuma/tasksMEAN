404491f717471087023d07d21b3b7bb28cf5c11c
KatrinaHuberJuma <katrina.huber@gmail.com>
2019-11-08 15:32:22 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+// =============================================
+// IMPORTS
+// =============================================
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+// =============================================
+// APP SETTINGS
+// =============================================
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/static"));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/client/views');
+// =============================================
+// CONNECT TO DATABASE (remember to rename)
+// =============================================
+mongoose.connect('mongodb://localhost/tasks', {useNewUrlParser: true});
+// =============================================
+// SCHEMA
+// =============================================
+
+const TaskSchema = new mongoose.Schema({
+    title: { type: String,
+        minlength: 3
+    },
+    description: { type: String,
+        default: ""
+    },
+    completed: {type: Boolean, 
+        default: false
+    }
+}, {timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }})
+
+
+
+const Task = mongoose.model('Task', TaskSchema);
+
+
+app.get('/tasks', (request, response) => {
+    Task.find({})
+        .then(tasks => response.json(tasks))
+        .catch(err => response.json(err));
+
+});
+
+app.get('/tasks/:id', (request, response) => {
+    console.log(request.params.id)
+    Task.find({_id:request.params.id})
+        .then(task => response.json(task))
+        .catch(err => response.json(err));
+
+});
+
+app.put('/tasks/:id', (request, response) => {
+    
+    Task.updateOne({_id:request.params.id}, {$set: request.body})
+        .then(task => response.json(task))
+        .catch(err => response.json(err));
+
+});
+
+app.delete('/tasks/:id', (request, response) => {
+    
+    Task.deleteOne({_id:request.params.id}, {$set: request.body})
+        .then(task => response.json(task))
+        .catch(err => response.json(err));
+
+});
+
+
+app.post('/tasks', (request, response) => {
+        Task.create(request.body)
+            .then(tasks => response.json(tasks))
+            .catch(err => response.json(err)); 
+    
+});
+
+
+app.listen(8000, () => console.log("listening on port 8000"));
+
